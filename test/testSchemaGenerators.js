c1bb78394f3c8131672a167661c065c54d629cc3
Eric Capps <eric@decentmammal.com>
2014-05-08 17:20:27 -0500

    Add unit test for optional splat param

--- test/testSchemaGenerators.js
+++ test/testSchemaGenerators.js
@@ -1,27 +1,31 @@
 var assert = require('assert');
 var createGenerator = require('../index.js');
 
-suite('test the schema generators',function(){
+// returns a function which executes a generator when called. necessary for use with assert.throws
+var runGenerator = function(gen, params){
+	return function(){
+		gen(params)
+	}
+}
 
-	test('unparameterized route', function (done)  {
+suite('test the schema generators', function () {
+	test('unparameterized route', function () {
 		var schema = {
-			'path':'/dogs/cats/bears'
+			'path': '/dogs/cats/bears'
 		}
 
 		var gen = createGenerator(schema);
 
-		assert.equal(schema.path,gen(),'the schema should match the path it was generated with');
-		done();
+		assert.equal(gen(), schema.path, 'the schema should match the path it was generated with');
 	});
 
-	test('parameterized route without options',function(done){
-
+	test('parameterized route without options', function () {
 		var schema = {
-			'path':'/hello/:name',
-			'params':{
-				'name':{
-					'type':'path',
-					'values':[
+			'path': '/hello/:name',
+			'params': {
+				'name': {
+					'type': 'path',
+					'values': [
 						'charles',
 						'barnabus'
 					]
@@ -30,80 +34,75 @@ suite('test the schema generators',function(){
 		};
 
 		var gen = createGenerator(schema);
-		var runGen = function(params){
-			return function(){
-				gen(params)
-			}
-		}
 
-		assert.equal('/hello/charles',gen({'name':'charles'}),'the schema should match a correct instance');
-		assert.throws(runGen({'name':'horsemeat'}), /value provided for a param is incorrect: name : horsemeat/, 'an incorrect param should throw');
-		assert.throws(runGen({'zzz':'horsemeat'}),/no value for a required param: name/, 'a missing param should throw');
-
-		done();
+		assert.equal(gen({'name':'charles'}), '/hello/charles', 'the schema should match a correct instance');
+		assert.throws(runGenerator(gen, {'name':'horsemeat'}), /value provided for a param is incorrect: name : horsemeat/ , 'an incorrect param should throw');
+		assert.throws(runGenerator(gen, {'zzz':'horsemeat'}), /no value for a required param: name/, 'a missing param should throw');
 	});
 
-	test('parameterized route with options',function(done){
+	test('parameterized route with optional components', function () {
 		var schema = {
-			'path':'/hello/:title?/:name',
-			'params':{
-				'name':{
-					'type':'path',
-					'values':[
-						'charles',
-						'barnabus'
-					]
-				},
-				'title':{
-					'type':'path',
-					'regex':'[a-z]+'
-				}
-			}
-		};
-
-		var gen = createGenerator(schema);
-		var runGen = function(params){
-			return function(){
-				gen(params)
-			}
-		}
-
-		assert.equal('/hello/doctor/barnabus',gen({'title':'doctor','name':'barnabus'}),'generator should work with optional params filled in');
-		assert.equal('/hello/charles',gen({'name':'charles'}),'generator should work without optional params filled in');
-
-		assert.throws(runGen({'title':'5555','name':'barnabus'}),/value provided for a param is incorrect: title : 5555/, 'an incorrect optional param should throw');
-
-		done();
-	});
-
-	test('parameterized route with splat',function(done){
-		var schema = {
-			'path':'/hello/:name/+extra',
-			'params':{
-				'name':{
-					'type':'path',
-					'values':[
+			'path': '/hello/:title?/:name',
+			'params': {
+				'name': {
+					'type': 'path',
+					'values': [
 						'charles',
 						'barnabus'
 					]
 				},
-				'title':{
-					'type':'path',
-					'regex':'\\w+'
+				'title': {
+					'type': 'path',
+					'regex': '[a-z]+'
 				}
 			}
 		};
 
 		var gen = createGenerator(schema);
-		var runGen = function(params){
-			return function(){
-				gen(params)
-			}
-		}
 
-		assert.equal('/hello/charles/a/b/c/d',gen({'name':'charles','extra':'a/b/c/d'}),'the gen should work with splat included');
-		assert.throws(runGen({'name':'barnabus'}),/no value for a required param: extra/, 'a missing splat param should throw');
+		assert.equal(gen({'title':'doctor', 'name':'barnabus'}), '/hello/doctor/barnabus', 'generator should work with optional params filled in');
+		assert.equal(gen({'name':'charles'}), '/hello/charles', 'generator should work without optional params filled in');
 
-		done();
+		assert.throws(runGenerator(gen, {'title':'5555','name':'barnabus'}), /value provided for a param is incorrect: title : 5555/, 'an incorrect optional param should throw');
 	});
+
+	test('parameterized route with required splat', function () {
+		var schema = {
+			'path': '/hello/:name/+extra',
+			'params': {
+				'name': {
+					'type': 'path',
+					'values': [
+						'charles',
+						'barnabus'
+					]
+				}
+			}
+		};
+
+		var gen = createGenerator(schema);
+		
+		assert.equal('/hello/charles/a/b/c/d', gen({'name':'charles','extra':'a/b/c/d'}), 'the gen should work with splat included');
+		assert.throws(runGenerator(gen, {'name':'barnabus'}), /no value for a required param: extra/, 'a missing splat param should throw');
+	});
+
+	test('parameterized route with optional splat', function () {
+		var schema = {
+			'path': '/hello/:name/*extra',
+			'params': {
+				'name': {
+					'type': 'path',
+					'values': [
+						'charles',
+						'barnabus'
+					]
+				}
+			}
+		};
+
+		var gen = createGenerator(schema);
+		
+		assert.equal('/hello/charles/a/b/c/d', gen({'name':'charles','extra':'a/b/c/d'}), 'the gen should work with splat included');
+		assert.equal('/hello/barnabus', gen({'name':'barnabus'}), 'a missing optional splat is fine');
+	})
 });
